753efb0526b47a4dc60f44091b3ceea82978a9af
KatiePDX <61799774+katiepdx@users.noreply.github.com>
2020-07-08 15:10:32 -0700

    grab DOM elements

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
 // import functions and grab DOM elements
+const gameTiles = document.querySelector('.game-tiles');
+console.log(gameTiles);
+
+const playButton = document.querySelector('.button');
+
+console.log(playButton);
+const gameResults = document.querySelector('.game-results');
+
+console.log(gameResults);
+const roundResults = document.querySelector('.round-results');
+console.log(roundResults);
+const computersResults = document.querySelector('.computers-result');
+console.log(computersResults);
+const playersWins = document.querySelector('.players-wins');
+console.log(playersWins);
+const playersLosses = document.querySelector('.players-losses');
+console.log(playersLosses);
+const totalDraws = document.querySelector('.total-draws');
+console.log(totalDraws);
 
 // initialize state
+
 
 // set event listeners to update state and DOM
---
+++ gameUtils.js
@@ -0,0 +1,8 @@
+// functions to export go here
+export function getRandomThrow {
+    //do this
+}
+
+export function checkResult {
+    //do this
+}
