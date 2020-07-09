0166f242bb51149802ad68b585c3a7d348ad36da
Frank Josh Olloqui <josh.olloqui@gmail.com>
2020-07-09 15:07:17 -0700

    this commit refactors and refines html, and js

--- gameUtils.js
+++ gameUtils.js
@@ -1,28 +1,17 @@
 // functions to export go here
-
-//this will create a random number
-    // If random number is between 0-0.33, return rock
-    // If random number is between 0.33-0.66, return paper
-    // If random number is between 66-1, return scissors
-   
 export function getRandomThrow(){
     let randomNumber = Math.random();
     if ((randomNumber > 0) && (randomNumber < 0.33)) {
-        console.log('rock');
         return 'rock';
     } else if ((randomNumber > 0.33) && (randomNumber < 0.66)) {
-        console.log('paper');
         return 'paper';
     } else if ((randomNumber > 0.33) && (randomNumber < 1)) {
-        console.log('scissors');
         return 'scissors';
     }
 }
-
-// This function will take two arguments (computers choice  and players choice) and check who wins using if statements 
-
+ 
 export function checkResult(computersChoice, playersChoice) {
-    if (computersChoice === 'rock' && playersChoice === 'rock') {
+    if (computersChoice === playersChoice) {
         return ('It is a draw!');
     } else if (computersChoice === 'paper' && playersChoice === 'rock') {
         return ('You lose!');
@@ -30,15 +19,11 @@ export function checkResult(computersChoice, playersChoice) {
         return ('You win!');
     } else if (computersChoice === 'rock' && playersChoice === 'paper') {
         return ('You win!'); 
-    } else if (computersChoice === 'paper' && playersChoice === 'paper') {
-        return ('It is a draw!'); 
     } else if (computersChoice === 'scissors' && playersChoice === 'paper') {
         return ('You lose!'); 
     } else if (computersChoice === 'rock' && playersChoice === 'scissors') {
         return ('You lose!');
     } else if (computersChoice === 'paper' && playersChoice === 'scissors') {
         return ('You win!'); 
-    } else if (computersChoice === 'scissors' && playersChoice === 'scissors') {
-        return ('It is a draw!');   
     }
 }
--- test/gameUtils.test.js
+++ test/gameUtils.test.js
@@ -3,36 +3,36 @@ import { getRandomThrow, checkResult } from '../gameUtils.js';
 
 const test = QUnit.test;
 
-test('this will test if the getRandomThrow function returns a string', (expect) => {
+test('this will test that the getRandomThrow function returns a string', (expect) => {
     //Arrange
-    // Set up your arguments and expectations
-    const expected1 = 'string';
+    // Expected result
+    const expected = 'string';
     
     //Act 
-    // Call the function you're testing and set the result to a const
-    const actual1 = typeof(getRandomThrow());
+    // Variable actual calls operator typeof for function getRandomThrow
+    const actual = typeof(getRandomThrow());
 
     //Expect
-    // Make assertions about what is expected versus the actual result
-    expect.equal(actual1, expected1);
+    // Actual will return 'string' and match expected
+    expect.equal(actual, expected);
 });
 
 test('this will test if the checkResult function logic is correct for all ROCK scenarios', (expect) => {
     //Arrange
-    // Set up your arguments and expectations
+    // Setting possible user choices to variables and adding fixed computer choice
     const usersChoice1 = 'rock';
     const usersChoice2 = 'paper';
     const usersChoice3 = 'scissors';
     const fixedComputersChoice = 'rock';
     
     //Act 
-    // Call the function you're testing and set the result to a const
+    // Using function checkResult to determine actual
     const actual1 = checkResult(fixedComputersChoice, usersChoice1);
     const actual2 = checkResult(fixedComputersChoice, usersChoice2);
     const actual3 = checkResult(fixedComputersChoice, usersChoice3);
 
     //Expect
-    // Make assertions about what is expected versus the actual result
+    // Comparing actual with the predicted results
     expect.equal(actual1, 'It is a draw!');
     expect.equal(actual2, 'You win!');
     expect.equal(actual3, 'You lose!');
@@ -40,20 +40,20 @@ test('this will test if the checkResult function logic is correct for all ROCK s
 
 test('this will test if the checkResult function logic is correct for all PAPER scenarios', (expect) => {
     //Arrange
-    // Set up your arguments and expectations
+    // Setting possible user choices to variables and adding fixed computer choice
     const usersChoice1 = 'rock';
     const usersChoice2 = 'paper';
     const usersChoice3 = 'scissors';
     const fixedComputersChoice = 'paper';
     
     //Act 
-    // Call the function you're testing and set the result to a const
+    // Using function checkResult to determine actual
     const actual1 = checkResult(fixedComputersChoice, usersChoice1);
     const actual2 = checkResult(fixedComputersChoice, usersChoice2);
     const actual3 = checkResult(fixedComputersChoice, usersChoice3);
 
     //Expect
-    // Make assertions about what is expected versus the actual result
+    // Comparing actual with the predicted results
     expect.equal(actual1, 'You lose!');
     expect.equal(actual2, 'It is a draw!');
     expect.equal(actual3, 'You win!');
@@ -61,20 +61,20 @@ test('this will test if the checkResult function logic is correct for all PAPER 
 
 test('this will test if the checkResult function logic is correct for all SCISSORS scenarios', (expect) => {
     //Arrange
-    // Set up your arguments and expectations
+    // Setting possible user choices to variables and adding fixed computer choice
     const usersChoice1 = 'rock';
     const usersChoice2 = 'paper';
     const usersChoice3 = 'scissors';
     const fixedComputersChoice = 'scissors';
     
     //Act 
-    // Call the function you're testing and set the result to a const
+    // Using function checkResult to determine actual
     const actual1 = checkResult(fixedComputersChoice, usersChoice1);
     const actual2 = checkResult(fixedComputersChoice, usersChoice2);
     const actual3 = checkResult(fixedComputersChoice, usersChoice3);
 
     //Expect
-    // Make assertions about what is expected versus the actual result
+    // Comparing actual with the predicted results
     expect.equal(actual1, 'You win!');
     expect.equal(actual2, 'You lose!');
     expect.equal(actual3, 'It is a draw!');
