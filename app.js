9a253b490a4184e99240377cb75777a12b273ba0
KatiePDX <61799774+katiepdx@users.noreply.github.com>
2020-07-08 17:18:31 -0700

    add game logic and update global state

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import { getRandomThrow } from './gameUtils.js'
+import { getRandomThrow, checkResult } from './gameUtils.js';
 
 // import functions and grab DOM elements
 const gameTiles = document.querySelector('.game-tiles');
@@ -7,9 +7,8 @@ console.log(gameTiles);
 const playButton = document.querySelector('.button');
 
 console.log(playButton);
-const gameResults = document.querySelector('.game-results');
-
-console.log(gameResults);
+const gameData = document.querySelector('.game-data');
+console.log(gameData);
 const roundResults = document.querySelector('.round-results');
 console.log(roundResults);
 const computersResults = document.querySelector('.computers-result');
@@ -22,16 +21,30 @@ const totalDraws = document.querySelector('.total-draws');
 console.log(totalDraws);
 
 // Global State 
-// let userWins = 0;
-// let userLosses = 0;
-// let gameDraws = 0;
-
-let computersChoice = getRandomThrow();
+let userWins = 0;
+let userLosses = 0;
+let gameDraws = 0;
 
 // initialize state
 playButton.addEventListener('click', () => {
-    computersResults.textContent = getRandomThrow();
-
+    let computersChoice = getRandomThrow();
+    let playerChecked = document.querySelector('input:checked');
+    let playersChoice = playerChecked.value;
+    computersResults.textContent = computersChoice;
+    
+    let oneRoundResult = checkResult(computersChoice, playersChoice);
+    roundResults.textContent = oneRoundResult;
+    if (oneRoundResult === 'You win!') {
+        userWins = userWins + 1;
+        console.log(userWins);
+        playersWins.textContent = userWins;
+    } else if (oneRoundResult === 'You lose!') {
+        userLosses = userLosses + 1;
+        playersLosses.textContent = userLosses;
+    } else if (oneRoundResult === 'It is a draw!') {
+        gameDraws = gameDraws + 1;
+        totalDraws.textContent = gameDraws;
+    }
 });
 
 
